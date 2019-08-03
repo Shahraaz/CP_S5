d42ba71c673bc2c5b6e5a1b31ea5ac0f75692f44
Shahraaz Hussain <shahraazhussain@gmail.com>
2019-08-03 22:26:01 +0530

    Sat, Aug 03 22:26 Finishing Up

---
+++ Templates/Seive.cpp
@@ -0,0 +1,13 @@
+const int _NSeive = 2e5 + 10;
+vector<bool> isPrime(_NSeive, true);
+void buildSeive()
+{
+    isPrime[1] = false;
+    isPrime[0] = false;
+    for (int i = 4; i < _NSeive; i += 2)
+        isPrime[i] = false;
+    for (int i = 3; i * i < _NSeive; i += 3)
+        if (isPrime[i])
+            for (int j = i * i; j < _NSeive; j += i)
+                isPrime[j] = false;
+}
