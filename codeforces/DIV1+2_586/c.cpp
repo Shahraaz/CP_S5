5c53804a1730bdc4b3d09a9f5db8b498d681c690
Shahraaz Hussain <shahraazhussain@gmail.com>
2019-09-18 23:58:04 +0530

    Wed, Sep 18 23:58 Cf round Must improve thinking

---
+++ codeforces/DIV1+2_586/c.cpp
@@ -0,0 +1,89 @@
+//Optimise
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define multitest 1
+#ifdef WIN32
+#define db(...) ZZ(#__VA_ARGS__, __VA_ARGS__);
+#define pc(...) PC(#__VA_ARGS__, __VA_ARGS__);
+template <typename T, typename U>
+ostream &operator<<(ostream &out, const pair<T, U> &p)
+{
+	out << '[' << p.first << ", " << p.second << ']';
+	return out;
+}
+template <typename Arg>
+void PC(const char *name, Arg &&arg)
+{
+	std::cerr << name << " { ";
+	for (const auto &v : arg)
+		cerr << v << ' ';
+	cerr << " }\n";
+}
+template <typename Arg1>
+void ZZ(const char *name, Arg1 &&arg1)
+{
+	std::cerr << name << " = " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void ZZ(const char *names, Arg1 &&arg1, Args &&... args)
+{
+	const char *comma = strchr(names + 1, ',');
+	std::cerr.write(names, comma - names) << " = " << arg1;
+	ZZ(comma, args...);
+}
+#else
+#define db(...)
+#define pc(...)
+#endif
+
+using ll = long long;
+#define f first
+#define s second
+#define pb push_back
+const long long mod = 1000000007;
+auto TimeStart = chrono::steady_clock::now();
+
+const int nax = 2e5 + 10;
+
+void solve()
+{
+	string s;
+	cin >> s;
+	int n = s.length();
+	vector<int> Start[26];
+	for (int i = 0; i < n; ++i)
+	{
+		bool pr = true;
+		for (int j = 0; j < s[i] - 'a'; ++j)
+		{
+			if (Start[j].size())
+			{
+				cout << "Ann\n";
+				pr = false;
+				break;
+			}
+		}
+		if (pr)
+			cout << "Mike\n";
+		Start[s[i] - 'a'].pb(i);
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t = 1;
+#ifdef multitest
+	cin >> t;
+#endif
+	while (t--)
+		solve();
+#ifdef WIN32
+	cerr << "\n\nTime elapsed: " << chrono::duration<double>(chrono::steady_clock::now() - TimeStart).count() << " seconds.\n";
+	// cin >> t;
+	flush(cout);
+#endif
+	return 0;
+}
