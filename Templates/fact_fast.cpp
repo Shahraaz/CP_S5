ea29c03f6aeeb6df3b98789001987ad800a4d08b
Mohammed Shahraaz Hussain <shahraazhussain@gmail.com>
2020-02-10 20:09:09 +0530

    Mon Feb 10 20:09:09 IST 2020 AutoCommit

---
+++ Templates/fact_fast.cpp
@@ -0,0 +1,41 @@
+const int _n = 1e5 + 10;
+vector<int> Fact(_n), Inv(_n);
+const int kmod = 1000000007;
+
+int mul(int a, int b, int mod = kmod)
+{
+    return (long long)a * b % mod;
+}
+
+int add(int a, int b, int mod = kmod)
+{
+    a += b;
+    if (a >= mod)
+        a -= mod;
+    return a;
+}
+
+int sub(int a, int b, int mod = kmod)
+{
+    a -= b;
+    if (a < 0)
+        a += mod;
+    return a;
+}
+
+void pre()
+{
+    Inv[0] = Fact[0] = 1;
+    for (int i = 1; i < _n; ++i)
+    {
+        Fact[i] = mul(Fact[i - 1], i);
+        Inv[i] = mul(Inv[kmod % i], i - 1);
+    }
+}
+
+int ncr(int n, int r)
+{
+    if (n < 0 || r < 0 || n - r < 0)
+        return 0;
+    return mul(Fact[n], mul(Inv[r], Inv[n - r]));
+}
