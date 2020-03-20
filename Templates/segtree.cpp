60d25ac0618be214441ca2504cc0d8336b78684f
Mohammed Shahraaz Hussain <shahraazhussain@gmail.com>
2020-03-20 19:30:08 +0530

    Fri, Mar 20 19:30 AutoCommit

--- Templates/segtree.cpp
+++ Templates/segtree.cpp
@@ -1,3 +1,9 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
 struct Node
 {
 	long long val;
@@ -8,32 +14,41 @@ struct Node
 		a.val = max(a.val, rhs.val);
 		return a;
 	}
+	Node lazylazyMerge(const Node &rhs)
+	{
+		Node a = *this;
+		a.val = max(a.val, rhs.val);
+		return a;
+	}
+	Node seglazyMerge(const Node &rhs)
+	{
+		Node a = *this;
+		a.val = max(a.val, rhs.val);
+		return a;
+	}
+	Node segSegMerge(const Node &rhs)
+	{
+		Node a = *this;
+		a.val = max(a.val, rhs.val);
+		return a;
+	}
 };
-ostream &operator<<(ostream &out, const Node &p)
-{
-	out << p.val;
-	return out;
-}
-istream &operator>>(istream &in, Node &p)
-{
-	in >> p.val;
-	return in;
-}
 
+template <typename segNode>
 struct Segtree
 {
-	vector<Node> Seg, Lazy;
-	vector<Node> Base;
-	vector<bool> isLazy;
+	vector<segNode> Seg, Lazy;
+	vector<segNode> Base;
+	vector<segNode> isLazy;
 	int n;
-	Segtree(int n = 2e5)
+	Segtree(int _n = 2e5)
 	{
-		this->n = n;
-		Seg.resize(4 * n + 10);
-		Lazy.resize(4 * n + 10);
-		isLazy.resize(4 * n + 10);
+		this->n = _n;
+		Seg.resize(4 * _n + 10);
+		Lazy.resize(4 * _n + 10);
+		isLazy.resize(4 * _n + 10);
 	}
-	void merge(Node &curr, Node &l, Node &r)
+	void merge(segNode &curr, segNode &l, segNode &r)
 	{
 		curr = l + r;
 	}
@@ -42,15 +57,15 @@ struct Segtree
 		if (isLazy[node])
 		{
 			isLazy[node] = false;
-			Seg[node] = Seg[node] + Lazy[node];
+			Seg[node] = Seg[node].seglazyMerge(Lazy[node]);
 			if (L != R)
 			{
-				Lazy[2 * node] = Lazy[2 * node] + Lazy[node];
-				Lazy[2 * node + 1] = Lazy[2 * node + 1] + Lazy[node];
+				Lazy[2 * node] = Lazy[2 * node].lazylazyMerge(Lazy[node]);
+				Lazy[2 * node + 1] = Lazy[2 * node + 1].lazylazyMerge(Lazy[node]);
 				isLazy[2 * node] = true;
 				isLazy[2 * node + 1] = true;
 			}
-			Lazy[node] = Node();
+			Lazy[node] = segNode();
 		}
 	}
 	void build(int node, int start, int end)
@@ -63,9 +78,9 @@ struct Segtree
 		int mid = (start + end) / 2;
 		build(2 * node, start, mid);
 		build(2 * node + 1, mid + 1, end);
-		merge(Seg[node], Seg[2 * node], Seg[2 * node + 1]);
+		Seg[node] = Seg[2 * node].segSegMerge(Seg[2 * node + 1]);
 	}
-	void build(vector<Node> &Arr)
+	void build(vector<segNode> &Arr)
 	{
 		Base = Arr;
 		n = Arr.size();
@@ -74,21 +89,19 @@ struct Segtree
 		isLazy.resize(4 * n + 10);
 		build(1, 0, n - 1);
 	}
-	Node Query(int node, int start, int end, int qstart, int qend)
+	segNode Query(int node, int start, int end, int qstart, int qend)
 	{
 		propagate(node, start, end);
 		if (qend < start || qstart > end || start > end)
-			return Node();
+			return segNode();
 		if (qstart <= start && end <= qend)
 			return Seg[node];
 		int mid = (start + end) / 2;
-		Node l = Query(2 * node, start, mid, qstart, qend);
-		Node r = Query(2 * node + 1, mid + 1, end, qstart, qend);
-		Node ret;
-		merge(ret, l, r);
-		return ret;
+		segNode l = Query(2 * node, start, mid, qstart, qend);
+		segNode r = Query(2 * node + 1, mid + 1, end, qstart, qend);
+		return l.segSegMerge(r);
 	}
-	Node qQuery(int node, int start, int end, int pos)
+	segNode qQuery(int node, int start, int end, int pos)
 	{
 		propagate(node, start, end);
 		if (start == end)
@@ -98,7 +111,7 @@ struct Segtree
 			return qQuery(2 * node, start, mid, pos);
 		return qQuery(2 * node + 1, mid + 1, end, pos);
 	}
-	void Update(int node, int start, int end, int qstart, int qend, Node val)
+	void Update(int node, int start, int end, int qstart, int qend, segNode val)
 	{
 		propagate(node, start, end);
 		if (qend < start || qstart > end || start > end)
@@ -113,15 +126,14 @@ struct Segtree
 		int mid = (start + end) / 2;
 		Update(2 * node, start, mid, qstart, qend, val);
 		Update(2 * node + 1, mid + 1, end, qstart, qend, val);
-		merge(Seg[node], Seg[2 * node], Seg[2 * node + 1]);
+		Seg[node] = Seg[2 * node].segSegMerge(Seg[2 * node + 1]);
 	}
-	void pUpdate(int node, int start, int end, int pos, Node val)
+	void pUpdate(int node, int start, int end, int pos, segNode val)
 	{
 		propagate(node, start, end);
 		if (start == end)
 		{
 			isLazy[node] = true;
-			Seg[node] = Node();
 			Lazy[node] = val;
 			propagate(node, start, end);
 			return;
@@ -131,22 +143,28 @@ struct Segtree
 			pUpdate(2 * node, start, mid, pos, val);
 		else
 			pUpdate(2 * node + 1, mid + 1, end, pos, val);
-		merge(Seg[node], Seg[2 * node], Seg[2 * node + 1]);
+		Seg[node] = Seg[2 * node].segSegMerge(Seg[2 * node + 1]);
 	}
-	Node query(int pos)
+	segNode query(int pos)
 	{
 		return qQuery(1, 0, n - 1, pos);
 	}
-	Node query(int left, int right)
+	segNode query(int left, int right)
 	{
 		return Query(1, 0, n - 1, left, right);
 	}
-	void update(int pos, Node val)
+	void update(int pos, segNode val)
 	{
 		pUpdate(1, 0, n - 1, pos, val);
 	}
-	void update(int start, int end, Node val)
+	void update(int start, int end, segNode val)
 	{
 		Update(1, 0, n - 1, start, end, val);
 	}
 };
+
+int main()
+{
+	Segtree<Node> S(1e5);
+	return 0;
+}
--- codeforces/practice/DIV2_625E.cpp
+++ codeforces/practice/DIV2_625E.cpp
@@ -28,6 +28,165 @@ using Random = std::uniform_int_distribution<T>;
 
 const int NAX = 2e5 + 5, MOD = 1000000007;
 
+struct Node
+{
+    long long val;
+    Node(long long one = 0) : val(one) {}
+    Node lazylazyMerge(const Node &rhs)
+    {
+        Node a = *this;
+        a.val = (a.val + rhs.val);
+        return a;
+    }
+    Node seglazyMerge(const Node &rhs)
+    {
+        Node a = *this;
+        a.val = (a.val + rhs.val);
+        return a;
+    }
+    Node segSegMerge(const Node &rhs)
+    {
+        Node a = *this;
+        a.val = max(a.val, rhs.val);
+        return a;
+    }
+};
+
+template <typename segNode>
+struct Segtree
+{
+    vector<segNode> Seg, Lazy;
+    vector<segNode> Base;
+    vector<bool> isLazy;
+    int n;
+    Segtree(int _n = 2e5)
+    {
+        this->n = _n;
+        Seg.resize(4 * _n + 10);
+        Lazy.resize(4 * _n + 10);
+        isLazy.resize(4 * _n + 10);
+    }
+    void merge(segNode &curr, segNode &l, segNode &r)
+    {
+        curr = l + r;
+    }
+    void propagate(int node, int L, int R)
+    {
+        if (isLazy[node])
+        {
+            isLazy[node] = false;
+            Seg[node] = Seg[node].seglazyMerge(Lazy[node]);
+            if (L != R)
+            {
+                Lazy[2 * node] = Lazy[2 * node].lazylazyMerge(Lazy[node]);
+                Lazy[2 * node + 1] = Lazy[2 * node + 1].lazylazyMerge(Lazy[node]);
+                isLazy[2 * node] = true;
+                isLazy[2 * node + 1] = true;
+            }
+            Lazy[node] = segNode();
+        }
+    }
+    void build(int node, int start, int end)
+    {
+        if (start == end)
+        {
+            Seg[node] = Base[start];
+            return;
+        }
+        int mid = (start + end) / 2;
+        build(2 * node, start, mid);
+        build(2 * node + 1, mid + 1, end);
+        Seg[node] = Seg[2 * node].segSegMerge(Seg[2 * node + 1]);
+    }
+    void build(vector<segNode> &Arr)
+    {
+        Base = Arr;
+        n = Arr.size();
+        Seg.resize(4 * n + 10);
+        Lazy.resize(4 * n + 10);
+        isLazy.resize(4 * n + 10);
+        build(1, 0, n - 1);
+    }
+    segNode Query(int node, int start, int end, int qstart, int qend)
+    {
+        propagate(node, start, end);
+        if (qend < start || qstart > end || start > end)
+            return segNode();
+        if (qstart <= start && end <= qend)
+            return Seg[node];
+        int mid = (start + end) / 2;
+        segNode l = Query(2 * node, start, mid, qstart, qend);
+        segNode r = Query(2 * node + 1, mid + 1, end, qstart, qend);
+        return l.segSegMerge(r);
+    }
+    segNode qQuery(int node, int start, int end, int pos)
+    {
+        propagate(node, start, end);
+        if (start == end)
+            return Seg[node];
+        int mid = (start + end) / 2;
+        if (pos <= mid)
+            return qQuery(2 * node, start, mid, pos);
+        return qQuery(2 * node + 1, mid + 1, end, pos);
+    }
+    void Update(int node, int start, int end, int qstart, int qend, segNode val)
+    {
+        propagate(node, start, end);
+        if (qend < start || qstart > end || start > end)
+            return;
+        if (qstart <= start && end <= qend)
+        {
+            isLazy[node] = true;
+            Lazy[node] = val;
+            propagate(node, start, end);
+            return;
+        }
+        int mid = (start + end) / 2;
+        Update(2 * node, start, mid, qstart, qend, val);
+        Update(2 * node + 1, mid + 1, end, qstart, qend, val);
+        Seg[node] = Seg[2 * node].segSegMerge(Seg[2 * node + 1]);
+    }
+    void pUpdate(int node, int start, int end, int pos, segNode val)
+    {
+        propagate(node, start, end);
+        if (start == end)
+        {
+            isLazy[node] = true;
+            Lazy[node] = val;
+            propagate(node, start, end);
+            return;
+        }
+        int mid = (start + end) / 2;
+        if (pos <= mid)
+            pUpdate(2 * node, start, mid, pos, val);
+        else
+            pUpdate(2 * node + 1, mid + 1, end, pos, val);
+        Seg[node] = Seg[2 * node].segSegMerge(Seg[2 * node + 1]);
+    }
+    segNode query(int pos)
+    {
+        return qQuery(1, 0, n - 1, pos);
+    }
+    segNode query(int left, int right)
+    {
+        return Query(1, 0, n - 1, left, right);
+    }
+    void update(int pos, segNode val)
+    {
+        pUpdate(1, 0, n - 1, pos, val);
+    }
+    void update(int start, int end, segNode val)
+    {
+        Update(1, 0, n - 1, start, end, val);
+    }
+};
+
+int n, m, k;
+ll v1[1000111];
+ll v2[1000111];
+const int M = 1000001;
+vector<pair<int, int>> qv[1000111];
+
 class Solution
 {
 private:
@@ -36,6 +195,46 @@ public:
     ~Solution() {}
     void solveCase()
     {
+        cin >> n >> m >> k;
+        for (size_t i = 0; i < M; i++)
+            v1[i] = v2[i] = -1e18;
+        for (size_t i = 0; i < n; i++)
+        {
+            ll x, y;
+            cin >> x >> y;
+            v1[x] = max(v1[x], -y);
+        }
+        for (size_t i = 0; i < m; i++)
+        {
+            ll x, y;
+            cin >> x >> y;
+            v2[x] = max(v2[x], -y);
+        }
+        Segtree<Node> S(M);
+        for (size_t i = 0; i < M; i++)
+            S.update(i, Node(v2[i]));
+        for (size_t i = 1; i <= k; i++)
+        {
+            int x, y, c;
+            cin >> x >> y >> c;
+            qv[x].pb({y, c});
+        }
+        ll ans = -1e18;
+        // db("a");
+        for (size_t i = 0; i < M; i++)
+        {
+            // db(i);
+            ans = max(ans, v1[i] + S.query(0, M).val);
+            // db(ans);
+            for (auto &x : qv[i])
+            {
+                // cout << S.query(0, M).val << ' ';
+                // cout << ans << ' ';
+                // cout << i << '\n';
+                S.update(x.f + 1, M, Node(x.s));
+            }
+        }
+        cout << ans << '\n';
     }
 };
 
